3acc76497308ffdb3feeb7a20436961889c538be
Amber Hall <ahall@redventures.com>
2024-12-10 11:32:34 -0800

    second part

--- day_1/app.js
+++ day_1/app.js
@@ -22,6 +22,7 @@
 // };
 
 const fs = require("fs");
+const obj = {};
 
 const parseInputFile = (filePath) => {
   const data = fs.readFileSync(filePath, "utf-8");
@@ -36,6 +37,8 @@ const parseInputFile = (filePath) => {
       const [key, value] = line.split(/\s+/);
       list1.push(key);
       list2.push(value);
+      obj[key] = 0;
+
     }
   });
 
@@ -48,16 +51,27 @@ const returnDifferenceOfArrays = (arr, arr2) => {
   const sortedArr1 = arr.sort((a, b) => a - b);
   const sortedArr2 = arr2.sort((a, b) => a - b);
   let res = 0;
+  let sims = 0;
+  
 
   for (let i = 0; i < sortedArr1.length; i++) {
     let differences = sortedArr2[i] - sortedArr1[i];
-    console.log(
-      sortedArr1[i] + " - " + sortedArr2[i] + " = " + Math.abs(differences)
-    );
+    
+
+    if (Object.keys(obj).includes(sortedArr2[i])) {
+      obj[sortedArr2[i]]++;
+    }
+
+    // console.log(
+    //   sortedArr1[i] + " - " + sortedArr2[i] + " = " + Math.abs(differences)
+    // );
 
     res +=  Math.abs(differences);
   }
+  Object.keys(obj).map((k) => {sims += obj[k] * k});
+  console.log(sims, "sims");
   return res;
 };
 
-console.log(returnDifferenceOfArrays(list1, list2));
+returnDifferenceOfArrays(list1, list2);
+// console.log(obj, "obj");
